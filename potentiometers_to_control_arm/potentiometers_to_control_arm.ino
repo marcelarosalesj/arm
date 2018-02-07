084075415d98e7b326680830d08242663bd8224d
Marcela Rosales <marcelarosalesj@gmail.com>
2018-02-06 22:20:16 -0600

    change to 5 servos due to issues with the current supply (previous problems were solved with a more powerful power souce, it can provide 30A) also I limited the servos movement to not harm my robot and its cables, I used the map function to map the read value (0-1023) to a limited range of degrees (the limit depends on each servo)

--- potentiometers_to_control_arm/potentiometers_to_control_arm.ino
+++ potentiometers_to_control_arm/potentiometers_to_control_arm.ino
@@ -1,86 +1,78 @@
 #include <Servo.h>
 
 Servo servo0;
-int pos0 = 0;
 Servo servo1;
-int pos1 = 0;
 Servo servo2;
-int pos2 = 0;
 Servo servo3;
-int pos3 = 0;
 Servo servo4;
-int pos4 = 0;
-Servo servo5;
-int pos5 = 0;
+//Servo servo5;
 
-double factor = (180.0/1024.0);
+int pot0 = A0;
+int pot1 = A1;
+int pot2 = A2;
+int pot3 = A3;
+int pot4 = A4;
+//int pot5 = A5;
+
+int val0 = 0;
+int val1 = 0;
+int val2 = 0;
+int val3 = 0;
+int val4 = 0;
+//int val5 = 0;
+
 
 void setup() {
   // put your setup code here, to run once:
   Serial.begin(9600);
+
   servo0.attach(13);
+  servo0.write(90);
+
   servo1.attach(12);
+  servo1.write(90);
+
   servo2.attach(11);
+  servo2.write(90);
+
   servo3.attach(10);
+  servo3.write(90);
+
   servo4.attach(9);
-  servo5.attach(8);
+  servo4.write(90);
+
+  //servo5.attach(8);
+  //servo5.write(90);
 
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
 
-  
-  int pot0 = analogRead(A0);
-  Serial.print("Potentiometer 0 = ");
-  Serial.println(pot0);
-  pos0 = factor*pot0;
-  Serial.print("Degrees0 = ");
-  Serial.println(pos0);
-  servo0.write(pos0);
-  
-  int pot1 = analogRead(A1);
-  Serial.print("Potentiometer 1 = ");
-  Serial.println(pot1);
-  pos1 = factor*pot1;
-  Serial.print("Degrees1 = ");
-  Serial.println(pos1);
-  servo1.write(pos1);
-  
-  
-  int pot2 = analogRead(A2);
-  Serial.print("Potentiometer 2 = ");
-  Serial.println(pot2);
-  pos2 = factor*pot2;
-  Serial.print("Degrees2 = ");
-  Serial.println(pos2);
-  servo2.write(pos2);
-  
-  
-  int pot3 = analogRead(A3);
-  Serial.print("Potentiometer 3 = ");
-  Serial.println(pot3);
-  pos3 = factor*pot3;
-  Serial.print("Degrees3 = ");
-  Serial.println(pos3);
-  servo3.write(pos3);
-  
-  
-  int pot4 = analogRead(A4);
-  Serial.print("Potentiometer 4 = ");
-  Serial.println(pot4);
-  pos4 = factor*pot4;
-  Serial.print("Degrees4 = ");
-  Serial.println(pos4);
-  servo4.write(pos4);
-  
-  int pot5 = analogRead(A5);  
-  Serial.print("Potentiometer 5 = ");
-  Serial.println(pot5);
-  pos5 = factor*pot5;  
-  Serial.print("Degrees5 = ");
-  Serial.println(pos5);
-  servo5.write(pos5);
-  
+  val0 = analogRead(pot0);
+  val0 = map(val0, 0, 1023, 0, 180);
+  servo0.write(val0);
+
+  val1 = analogRead(pot1);
+  val1 = map(val1, 0, 1023, 10, 170);
+  servo1.write(val1);
+
+  val2 = analogRead(pot2);
+  val2 = map(val2, 0, 1023, 20, 90);
+  servo2.write(val2);
+
+  val3 = analogRead(pot3);
+  val3 = map(val3, 0, 1023, 10, 100);
+  servo3.write(val3);
+
+  val4 = analogRead(pot4);
+  val4 = map(val4, 0, 1023, 20, 140);
+  servo4.write(val4);
+
+  /*val5 = analogRead(pot5);
+    val5 = map(val5, 0, 1023, 0, 180);
+    servo5.write(val5);
+    //delay(15);*/
+
+  delay(100);
 
 }
